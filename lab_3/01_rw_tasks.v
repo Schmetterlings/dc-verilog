7d0bfa1f06308371367d8a1fe8f4b7beb6511027
JaskulaPrzemek <49696578+JaskulaPrzemek@users.noreply.github.com>
2020-05-19 15:00:41 +0200

    My implementation, probalby working

--- lab_3/01_rw_tasks.v
+++ lab_3/01_rw_tasks.v
@@ -1,4 +1,4 @@
-// Performed by: Marut Kamil
+// Perforemd by: {Full Last,First Name}
 //
 // Task 1. Build from RAM_16X4 a memory module equipped with bidirectional 
 // data port D. Write a testbench that verifies the operation of reading 
@@ -10,6 +10,7 @@
 // the correct driving of bus lines to avoid conflicts and assure the proper 
 // sequence of signals. In the testbench write a test of memory operation 
 // using prepared tasks
+//
 
 `timescale 1ns/100ps
 
@@ -17,18 +18,15 @@ module RAM_16SP(nCS, nWE, nOE, A, D);
 input nCS, nWE, nOE;
 input [3:0] A;
 inout [3:0] D;
-
 wire [3:0] D_T;
+wire [3:0] D_T2;
+RAM_16X4 M1(.nCS(nCS), .nWE(nWE), .nOE(nOE), .A(A), .D(D_T), .Q(D_T2));
+assign  D_T = nOE ? D : 4'bz;
+assign  D = (~nOE&&nWE) ? D_T2 : 4'bz;
 
-RAM_16X4 M1(.nCS(nCS), .nWE(nWE), .nOE(nOE), .A(A), .D(D), .Q(D_T));
-
-// The correct memory is loaded into D_T
-// but for some reason the continuous assignment
-// does not assign the D_T to D
-assign #10 D = (~nOE && nWE) ? D_T : 4'bz;
 
 always @(A or D or D_T) begin
-    $display("%m: D -> %b, D_T -> %b", D, D_T);
+    $display("%m: D -> %b, D_T -> %b , D_T2 -> %b", D, D_T,D_T2);
 end
 
 endmodule
@@ -36,52 +34,35 @@ endmodule
 module MEM_TEST;
 
 reg nCS, nOE, nWE;
-reg [3:0] ADR;
-wire [3:0] MEM;
+reg [3:0] AD;
+wire [3:0] D;
 
 reg [3:0] D_DRV;
 reg [3:0] D_RD;
 
-RAM_16SP M1(.nCS(nCS), .nWE(nWE), .nOE(nOE), .A(ADR), .D(MEM));
+RAM_16SP M1(.nCS(nCS), .nWE(nWE), .nOE(nOE), .A(AD), .D(D));
 
 //For writing the data use D_DRV - remember about conflicts
-assign MEM = D_DRV;
+assign D = D_DRV;
 
 initial begin
-    #20; 
-    nCS = 1'b1; nWE = 1'b1; nOE = 1'b1;
-    ADR = 4'h0; D_DRV = 4'hx; D_RD = 4'hx;
-
-    // Writing memory
-    #10;
-    nCS = 1'b0;
-    #10;
-    nWE = 1'b0;
-    #10;
+    D_DRV = 4'hz;
+    AD=4'b0;
+    nCS=1;
+    nOE=1;
+    nWE=1;
+    $display("My testbench...");
+    //...?
+    MEM_WR(4'b0100, 4'b1100);
     MEM_WR(4'b0101, 4'b1110);
-    #10;
-    MEM_WR(4'b1010, 4'b0001);
-    #10;
-    nWE = 1'b1;
-
-    // Reading memory
-    #10;
-    nOE = 1'b0;
-    #10;
-    MEM_RD(4'b0101, D_RD);
-    #10;
-    MEM_RD(4'b1010, D_RD);
-    #10;
-    nOE = 1'b1;
-    #10;
-    nCS = 1'b1;
-    $display("Simulation finished.");
+    MEM_RD(4'b0100,D_RD);
+    MEM_RD(4'b0101,D_RD);
+    #100;
+    $display("Finish...");
     $finish;
 end
 
 initial begin
-    //$monitor("%t -> Chip select: %b, Write: %b, Output: %b - WR: %b, RD: %b", $time, nCS, nWE, nOE, D_DRV, D_RD);
-    $monitor("%t -> WR: %b, RD: %b", $time, D_DRV, D_RD);
     $dumpfile("mem_test.vcd");
     $dumpvars;
     $dumpon;
@@ -89,21 +70,40 @@ end
 
 task MEM_RD;
 input [3:0] A;
-output [3:0] D;
+output [3:0] D1;
 begin
-    $display("%m: Reading memory...");
-    #5 ADR = A;
-    #5 D = MEM;
-end
+    $display("Reading data...");
+    D_DRV=4'bz;
+    #10 ;
+    nCS=0;
+    AD=A;
+     nOE = 1'b0;
+    #15;
+    D1=D;
+    nOE = 1'b1;
+    #10;
+    nCS=1;
+    
+    end
 endtask
 
 task MEM_WR;
 input [3:0] A;
 input [3:0] D;
 begin
-    $display("%m: Writing memory...");
-    #5 ADR = A;
+    $display("Writing data...");
+    #10;
+     nCS = 1'b0;
+     #10;
+     AD=A;
     D_DRV = D;
+    nOE = 1'b1;
+    #10
+    nWE=1'b0;
+    #20;
+    nWE=1;
+    #5
+    nCS = 1'b1;    
 end
 endtask
 
