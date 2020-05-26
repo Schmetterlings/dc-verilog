ffc2beabf24721daefce4e672ee8899920efcb84
Kamil Marut <kakkmar@gmail.com>
2020-05-26 11:43:28 +0200

    Fix too early detection

--- lab_4/t2.v
+++ lab_4/t2.v
@@ -1,25 +1,25 @@
-// Perforemd by: {Full Last,First Name}
+// Performed by: Marut Kamil
 //
 // Task 2. Design an overflow detector by copping your frequency meter to prepared t2.v file. 
 // Here you will find module declaration including OVF (Overflow output). The overflow signal 
 // should be treated lie other measurement results and should be passed through the latch register
 // to retain the result value for displaying it for user.
-//
 
 `timescale 1ns/100ps
 
-module FM_OVF(CLK, CLR, F_IN, QH, QD, QU, OVF, nDONE);
+module FM_OVF(CLK, CLR, F_IN, QH, QD, QU, Q_OVF, nDONE);
 input CLK;  // Reference clock
 input CLR; // Frequency meter clear signal
 input F_IN; // Measured frequency 
 output [3:0] QH, QD, QU; // Measurement result
-output OVF; // Result overflow notification
+output Q_OVF; // Result overflow notification
 output nDONE; // Notification about completing measurement process
 
 // Control unit
 wire [3:0] Q_CTRL;
 wire [7:0] nCTRL;
 wire [1:0] DUMMY;
+wire OVF;
 wire GATE_EN; // Gate
 wire G_F_IN; // Gated F_IN
 wire CNT_CLR; // Counter clear
@@ -48,14 +48,17 @@ assign nDONE = nCTRL[7]; // End of measurement notification
 SN7490 C1(.CLK(G_F_IN), .R0(CNT_CLR), .R9(1'b0), .Q(QC_U));
 SN7490 C2(.CLK(QC_U[3]), .R0(CNT_CLR), .R9(1'b0), .Q(QC_D));
 SN7490 C3(.CLK(QC_D[3]), .R0(CNT_CLR), .R9(1'b0), .Q(QC_H));
+
+// Overflow detection
+wire nCNT_CLR;
+not #2(nCNT_CLR, CNT_CLR);
+SN7472 OVF_DETECT(.CLK(QC_H[3]), .nR(nCNT_CLR), .nS(1'b1), .J(1'b1), .K(1'b0), .Q(OVF), .nQ());
+
 // Latch - quad D flip-flop triggered on falling edge
 SN7474_4 L1(.CLK(LD), .nCLR(1'b1), .D(QC_U), .Q(QU));
 SN7474_4 L2(.CLK(LD), .nCLR(1'b1), .D(QC_D), .Q(QD));
 SN7474_4 L3(.CLK(LD), .nCLR(1'b1), .D(QC_H), .Q(QH));
-
-wire nCNT_CLR;
-not #2(nCNT_CLR, CNT_CLR);
-SN7472 OVF_DETECT(.CLK(QC_H[3]), .nR(nCNT_CLR), .nS(1'b1), .J(1'b1), .K(1'b0), .Q(OVF), .nQ());
+SN7474 L_OVF(.CLK(LD), .nR(1'b1), .nS(1'b1), .D(OVF), .Q(Q_OVF), .nQ());
 
 endmodule
 
@@ -72,7 +75,7 @@ reg [31:0] F_SET;
 FM_OVF UUT(
     .CLK(CLK), .CLR(CLR), 
     .F_IN(F_IN), 
-    .QH(QH), .QD(QD), .QU(QU), .OVF(OVF) ,
+    .QH(QH), .QD(QD), .QU(QU), .Q_OVF(OVF) ,
     .nDONE(nDONE));
 
 FREQ_GEN FG(.CLK(F_IN), .F(F_SET));
